b8f8c4afed333e996acfd276fbb512b03e673dd8
Maheshishere <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-06-23 00:51:04 +0530

    --build-fix

--- spaceview/app/page.tsx
+++ spaceview/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Layout from "../components/Layout";
-import NestedComponent from "../components/NestedComponent";
 
 export default function Home() {
 	return (
