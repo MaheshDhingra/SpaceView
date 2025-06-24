44bbd287f91ef2a147e6f9babeeb68a39ad57f92
Maheshishere <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-06-24 19:23:27 +0530

    --fix

--- spaceview/app/planets/[planet]/page.tsx
+++ spaceview/app/planets/[planet]/page.tsx
@@ -1,11 +1,7 @@
 import PlanetDetails from '../../../components/PlanetDetails';
 import Layout from '../../../components/Layout';
 
-export default function PlanetDetailsPage({
-  params,
-}: {
-  params: { planet: string };
-}) {
+export default function PlanetDetailsPage({ params }: { params: { planet: string } }) {
   return (
     <Layout>
       <PlanetDetails planetId={params.planet} />
